2c3013a442721e7822bcbacb6b38eb644e7e9899
sleepyfran <fgzv@outlook.com>
2024-10-24 23:36:47 +0200

    Initial Echo connector

---
+++ src/connectors/echo.ts
@@ -0,0 +1,70 @@
+export {};
+
+const retrievePlayerShadowRoot = () =>
+	document
+		.querySelector('app-root')
+		?.shadowRoot?.querySelector('app-header')
+		?.shadowRoot?.querySelector('echo-player')?.shadowRoot;
+
+Connector.getArtist = () => {
+	const player = retrievePlayerShadowRoot();
+	const artistElement = player?.querySelector('#artist-name');
+	const content = artistElement?.textContent;
+
+	return content;
+};
+
+Connector.getTrack = () => {
+	const player = retrievePlayerShadowRoot();
+	const trackElement = player?.querySelector('#track-name');
+	const content = trackElement?.textContent;
+
+	return content;
+};
+
+Connector.getTrackArt = () => {
+	const player = retrievePlayerShadowRoot();
+	const trackCover = player?.querySelector('#track-cover');
+	const src = trackCover?.attributes?.getNamedItem('src')?.textContent;
+
+	return src;
+};
+
+Connector.isPlaying = () => {
+	const player = retrievePlayerShadowRoot();
+	const pauseButton = player?.querySelector('#pause');
+
+	console.log('Pause button content', pauseButton);
+
+	return !!pauseButton;
+};
+
+const startObserving = (player: ShadowRoot) => {
+	const observer = new MutationObserver(() => {
+		Connector.onStateChanged();
+	});
+	const target = player.querySelector('.player');
+	if (!target) {
+		console.error(
+			'Somehow there was no play button, something must have changed!',
+		);
+		return;
+	}
+
+	observer.observe(target, {
+		childList: true,
+		subtree: true,
+	});
+	console.log('Finished setting up observer');
+};
+
+// Wait until the player is loaded and start observing.
+const timer = setInterval(() => {
+	const player = retrievePlayerShadowRoot();
+
+	if (player) {
+		console.log('Player loaded, setting up observer');
+		startObserving(player);
+		clearInterval(timer);
+	}
+}, 2000);
